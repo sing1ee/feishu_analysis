7f70c85e5d6b7da2a16877f667547c32869fe87b
zhangcheng <zh.milo@gmail.com>
2023-04-21 09:28:40 +0800

    black linter

--- server.py
+++ server.py
@@ -4,7 +4,12 @@ import os
 import logging
 import requests
 from api import MessageApiClient
-from event import MessageReceiveEvent, UrlVerificationEvent, MessageReadEvent, EventManager
+from event import (
+    MessageReceiveEvent,
+    UrlVerificationEvent,
+    MessageReadEvent,
+    EventManager,
+)
 from flask import Flask, jsonify
 from dotenv import load_dotenv, find_dotenv
 from utils import obj2dict
@@ -12,7 +17,7 @@ from utils import obj2dict
 # load env parameters form file named .env
 load_dotenv(find_dotenv())
 
-writer = open('event_data.txt', 'a')
+writer = open("event_data.txt", "a")
 
 app = Flask(__name__)
 
@@ -27,12 +32,14 @@ LARK_HOST = os.getenv("LARK_HOST")
 message_api_client = MessageApiClient(APP_ID, APP_SECRET, LARK_HOST)
 event_manager = EventManager()
 
+
 @event_manager.register("url_verification")
 def request_url_verify_handler(req_data: UrlVerificationEvent):
     # url verification, just need return challenge
     if req_data.event.token != VERIFICATION_TOKEN:
         raise Exception("VERIFICATION_TOKEN is invalid")
     return jsonify({"challenge": req_data.event.challenge})
+
 
 @event_manager.register("im.message.message_read_v1")
 def request_read_handler(req_data: MessageReadEvent):
@@ -44,7 +51,7 @@ def message_receive_event_handler(req_data: MessageReceiveEvent):
     sender_id = req_data.event.sender.sender_id
     message = req_data.event.message
     writer.write(json.dumps(obj2dict(req_data)))
-    writer.write('\n')
+    writer.write("\n")
     writer.flush()
     if message.message_type != "text":
         logging.warning("Other types of messages have not been processed yet")
@@ -71,7 +78,9 @@ def msg_error_handler(ex):
 @app.route("/", methods=["POST"])
 def callback_event_handler():
     # init callback instance and handle
-    event_handler, event = event_manager.get_handler_with_event(VERIFICATION_TOKEN, ENCRYPT_KEY)
+    event_handler, event = event_manager.get_handler_with_event(
+        VERIFICATION_TOKEN, ENCRYPT_KEY
+    )
     if not event_handler or not event:
         return jsonify()
     return event_handler(event)
